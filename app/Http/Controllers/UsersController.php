921cbc36b58810825e7c851c9fe8d9cd67696a1b
moore <1249973280@qq.com>
2017-11-29 14:48:54 +0000

    User profile page

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\user;
+
+class UsersController extends Controller
+{
+    //
+
+    public function show(User $user){
+
+      return view('users.show',compact('user'));
+    }
+}
