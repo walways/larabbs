2247039cb33e1fab7a63f48db17df0fec60e949d
moore <1249973280@qq.com>
2017-12-01 15:36:00 +0000

    User can create topic

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -13,6 +13,9 @@ class TopicRequest extends Request
             {
                 return [
                     // CREATE ROLES
+                    'title' => 'required|min:2',
+                    'body' => 'required|min:3',
+                    'category_id' => 'required|numeric',
                 ];
             }
             // UPDATE
@@ -21,6 +24,9 @@ class TopicRequest extends Request
             {
                 return [
                     // UPDATE ROLES
+                    'title' => 'required|min:2',
+                    'body' => 'required|min:3',
+                    'category_id' => 'required|numeric',
                 ];
             }
             case 'GET':
@@ -35,6 +41,8 @@ class TopicRequest extends Request
     public function messages()
     {
         return [
+            'title.min'=>'标题必须至少两个字符',
+            'body.min'=>'文章内容必须至少三个字符',
             // Validation messages
         ];
     }
